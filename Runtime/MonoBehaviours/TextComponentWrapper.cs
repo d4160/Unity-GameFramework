7443cf729096f0097c87eb013dea82d0f1d6afaf
d4160 <d4160@outlook.com>
2019-12-18 11:33:46 -0500

    Added TextComponentWrapper.

---
+++ Runtime/MonoBehaviours/TextComponentWrapper.cs
@@ -0,0 +1,34 @@
+﻿namespace d4160.Core
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using TMPro;
+
+    public class TextComponentWrapper : MonoBehaviour
+    {
+        protected Text m_UGUIText;
+        protected TextMeshProUGUI m_tmpUGUIText;
+
+        public Text UGUIText => m_UGUIText;
+        public TextMeshProUGUI TMPUGUIText => m_tmpUGUIText;
+
+        public virtual string Text
+        {
+            get {
+                return m_UGUIText ? m_UGUIText.text : m_tmpUGUIText?.text;
+            }
+            set {
+                if(m_UGUIText) m_UGUIText.text = value;
+                else if(m_tmpUGUIText) m_tmpUGUIText.text = value;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            m_UGUIText = this.GetComponent<Text>(true, true);
+
+            if (!m_UGUIText)
+                m_tmpUGUIText = this.GetComponent<TextMeshProUGUI>(true, true);
+        }
+    }
+}
