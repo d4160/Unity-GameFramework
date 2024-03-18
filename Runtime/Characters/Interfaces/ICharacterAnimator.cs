f7a58a1f728c09c174079fe21ab71a759e020c9e
d4160 <d4160@outlook.com>
2024-03-17 21:19:47 -0500

    New IsInState method to get the state of the animator

--- Runtime/Characters/Interfaces/ICharacterAnimator.cs
+++ Runtime/Characters/Interfaces/ICharacterAnimator.cs
@@ -5,5 +5,7 @@ namespace d4160.Characters
     {
         void PlayState(int index);
         void PlayTransition(int index, bool randomizeBlend = false, int maxBlendIndex = 1, float delay = 10f);
+
+        bool IsInState(int index);
     }
 }
